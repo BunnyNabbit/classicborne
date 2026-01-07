ee391217390e066ec6c20ad44dbb76468ee9c0cb
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2026-01-07 03:36:02 -0600

    Create index.test.mjs

---
+++ tests/index.test.mjs
@@ -0,0 +1,8 @@
+/** @file I exist for coverage. */
+import * as classicborne from "../index.mjs"
+
+describe("index", () => {
+	it("should import", () => {
+		expect(classicborne.BaseUniverse).toBeDefined()
+	})
+})
