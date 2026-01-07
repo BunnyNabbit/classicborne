fe42ed9b2b484c6153dbaf8779e1fa08390b0671
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2026-01-06 19:48:06 -0600

    Use prettier-plugin-drone-jsdoc

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,7 +1,11 @@
 /** @type {import("prettier").Config} */
 export default {
 	semi: false,
-	plugins: ["prettier-plugin-drone-class"],
+	plugins: ["prettier-plugin-drone-class", "prettier-plugin-drone-jsdoc"],
+	jsdocSeparateReturnsFromParam: false,
+	tsdoc: true,
+	jsdocTagsOrder: '{"todo":0}',
+	jsdocEmptyCommentStrategy: "keep",
 	printWidth: Infinity,
 	trailingComma: "es5",
 	useTabs: true,
