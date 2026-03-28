e1b8671c44413406e716975ca947fe48feebe611
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-03-27 20:16:03 -0500

    Update BaseTemplate documentation comments

--- class/level/BaseTemplate.mjs
+++ class/level/BaseTemplate.mjs
@@ -4,28 +4,46 @@ import { Buffer } from "node:buffer"
 
 /** @todo Yet to be documented. */
 export class BaseTemplate {
-	/**@todo Yet to be documented.
+	/**Create a new template with an icon and default bounds.
 	 *
-	 * @param {string} iconName
-	 * @param {Vector3} defaultBounds
+	 * @param {string} iconName - The name of the template.
+	 * @param {Vector3} defaultBounds - The default bounds for the template as [x, y, z].
 	 */
 	constructor(iconName, defaultBounds = [64, 64, 64]) {
 		if (!iconName) throw new Error("iconName not provided.")
+		/**The name of the template.
+		 *
+		 * @type {string}
+		 */
 		this.iconName = iconName
+		/**The default bounds for this template.
+		 *
+		 * @type {Vector3}
+		 */
 		this.defaultBounds = defaultBounds
 	}
-	/** @abstract */
-	generate() {
-		throw new Error("Template generate mezhod not implemented.")
+	/**Generate a level buffer. Subclasses must override this method.
+	 *
+	 * @abstract
+	 * @returns {Buffer}
+	 * @ts-ignore
+	 */
+	generate(bounds = this.defaultBounds) {
+		throw new Error("Template generate method not implemented.")
 	}
 }
-/** @todo Yet to be documented. */
+
+/** I'm a template that generates an empty level buffer of the specified size. */
 export class EmptyTemplate extends BaseTemplate {
-	/**/
+	/** Create an empty template with the default icon name. */
 	constructor() {
 		super("empty")
 	}
-
+	/**Generate an empty buffer for the given bounds.
+	 *
+	 * @param {Vector3} [bounds=this.defaultBounds] - The bounds for the empty level. Default is `this.defaultBounds`
+	 * @returns {Buffer}
+	 */
 	generate(bounds = this.defaultBounds) {
 		return Buffer.alloc(bounds[0] * bounds[1] * bounds[2])
 	}
