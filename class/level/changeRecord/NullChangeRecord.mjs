bcc84da3b34ca158c66ce6deb5ce21fc80770bc9
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-07-23 21:15:31 -0500

    1. Document NullChangeRecord
    Enable type-checking for NullChangeRecord

--- class/level/changeRecord/NullChangeRecord.mjs
+++ class/level/changeRecord/NullChangeRecord.mjs
@@ -1,10 +1,11 @@
+// @ts-check
 /** @import {Level} from "../BaseLevel.mjs" */
 /** @import {ChangeRecord} from "./ChangeRecord.mjs" */
 
 /** I'm a dummy change record for a {@link BaseLevel}. Unlike {@link ChangeRecord}, I do not keep track of changes. Instead, my methods are very minimal and don't do much in terms of operation. */
 export class NullChangeRecord {
 	/**/
-	constructor(path, loadedCallback = () => {}) {
+	constructor(path, loadedCallback = (/** @type {any} */ nullChangeRecord) => {}) {
 		this.currentBuffer = null
 		this.path = path
 		this.draining = false
@@ -17,25 +18,43 @@ export class NullChangeRecord {
 			loadedCallback(this)
 		}, 0)
 	}
-
+	/**No operation.
+	 *
+	 * @param {any} position
+	 * @param {any} block
+	 */
 	addBlockChange(position, block) {
 		this.appendAction(false, position.concat(block))
 	}
-
-	appendAction() {}
-
+	/**No operation.
+	 *
+	 * @param {any} isCommand
+	 * @param {any} actionBytes
+	 * @param {any} commandString
+	 */
+	appendAction(isCommand, actionBytes, commandString) {}
+	/**Returns `0`.
+	 *
+	 * @returns {Promise<0>}
+	 */
 	async restoreBlockChangesToLevel() {
 		return 0
 	}
-
+	/**Returns `0`.
+	 *
+	 * @returns {Promise<0>}
+	 */
 	async flushChanges() {
 		return 0
 	}
-
+	/**Returns `0`.
+	 *
+	 * @returns {Promise<0>}
+	 */
 	async commit() {
 		return 0
 	}
-
+	/** No operation. */
 	async dispose() {}
 	static lagKeyframeTime = 250 // milliseconds
 }
